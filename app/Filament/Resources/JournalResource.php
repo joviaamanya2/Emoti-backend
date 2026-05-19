e973fe489740a1d8b8ef16f232ec57fe0ccf8647
Amanyajovia <amanyajovia7@gmail.com>
2026-05-19 23:16:26 +0300

    changes

--- app/Filament/Resources/JournalResource.php
+++ app/Filament/Resources/JournalResource.php
@@ -3,57 +3,54 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\JournalResource\Pages;
+use App\Filament\Resources\JournalResource\RelationManagers;
 use App\Models\Journal;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class JournalResource extends Resource
 {
     protected static ?string $model = Journal::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-book-open';
-
-    protected static ?string $navigationGroup = 'User Content';
-
-    protected static ?string $navigationLabel = 'Journals';
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
 
     public static function form(Form $form): Form
     {
-        return $form->schema([
-            Forms\Components\Select::make('user_id')
-                ->relationship('user', 'name')
-                ->required(),
-
-            Forms\Components\TextInput::make('title')
-                ->required(),
-
-            Forms\Components\Textarea::make('content')
-                ->required(),
-        ]);
+        return $form
+            ->schema([
+                //
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('user.name')
-                    ->label('User'),
-
-                Tables\Columns\TextColumn::make('title')
-                    ->searchable(),
-
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime(),
+                //
+            ])
+            ->filters([
+                //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
     public static function getPages(): array
     {
         return [
@@ -61,5 +58,5 @@ class JournalResource extends Resource
             'create' => Pages\CreateJournal::route('/create'),
             'edit' => Pages\EditJournal::route('/{record}/edit'),
         ];
-    }
+    }    
 }
