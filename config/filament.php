a335d867abee33eab7e36151cd4c1d09210e5058
Amanyajovia <amanyajovia7@gmail.com>
2026-06-17 02:38:12 +0300

    dashboard more changes

--- config/filament.php
+++ config/filament.php
@@ -74,13 +74,21 @@ return [
 
     'brand' => env('APP_NAME', 'Emoti App'),
 
-    // Override default colors to use a green palette
-    'colors' => [
-        'primary' => \Filament\Support\Colors\Color::Emerald,
-        // Optional: change warning from orange to green as well
-        'warning' => \Filament\Support\Colors\Color::Emerald,
+        // Override default colors to use a green palette
+        'colors' => [
+        'primary' => [
+            50 => '#eff6ff',
+            100 => '#dbeafe',
+            200 => '#bfdbfe',
+            300 => '#93c5fd',
+            400 => '#60a5fa',
+            500 => '#3b82f6',
+            600 => '#2563eb',
+            700 => '#1d4ed8',
+            800 => '#1e40af',
+            900 => '#1e3a8a',
+        ],
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Auth
